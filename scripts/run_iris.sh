c020ce6e43871a250190ec7c8deb55938e8cebad
markotot <m.tot@qmul.ac.uk>
2025-01-31 14:28:14 +0000

    fix: fixed .def file changed Lightzero to iris

--- scripts/run_iris.sh
+++ scripts/run_iris.sh
@@ -2,8 +2,10 @@
 
 ENV_NAME=$1
 WANDB_API_KEY=$2
+
 wandb login $WANDB_API_KEY
 
+echo  "WANDB_API_KEY=$WANDB_API_KEY"
 echo "Training iris model on environment $ENV_NAME"
 python src/main.py env.train.id=$ENV_NAME common.device=cuda:0 wandb.mode=online
 wait
